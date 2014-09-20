3253675829e0b015227ac28e103962bb260f6470
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-20 16:32:13 -0700

    add example scripts

---
+++ environ.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# example python script
+
+import os
+
+for item in os.environ.items():
+    print item
---
+++ environ.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# example bash script
+
+echo "current working directory"
+pwd
+
+echo "environment"
+printenv
